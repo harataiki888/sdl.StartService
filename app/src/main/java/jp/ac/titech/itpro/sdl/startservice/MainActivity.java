b369d592e2d4af0454ab29e500b5c9c34154ea68
harataiki888 <hara.t.aq@m.titech.ac.jp>
2020-06-18 09:57:48 +0900

    exercise

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,20 +1,31 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+
+        registerReceiver(Receiver, new IntentFilter("ACTION_ANSWER"));
+
     }
 
     public void onClickTest1(View v) {
@@ -30,4 +41,38 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
         startService(intent);
     }
+
+    public void onClickTest3(View v) {
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
+        startService(intent);
+    }
+
+    BroadcastReceiver Receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "onReceive: " + action);
+            if (action == null) {
+                return;
+            }else if (action!=null){
+                Bundle bundle = intent.getExtras();
+                String message = bundle.getString("EXTRA_ANSWER");
+                Log.d(TAG, message);
+                Toast ts= Toast.makeText( context, message, Toast.LENGTH_LONG);
+                ts.setGravity(Gravity.CENTER, 0, 0);
+                ts.show();
+            }
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(Receiver);
+    }
+
 }
+
